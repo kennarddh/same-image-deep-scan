421b71ace27296862edad0ce74509d81cb7e0c5a
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-16 13:39:55 +0700

    feat: Compare image func

---
+++ src/CompareImage.ts
@@ -0,0 +1,12 @@
+import { IImage } from 'Types.js'
+
+const CompareImage = (image1: IImage, image2: IImage): boolean => {
+	if (image1.path === image2.path) return true
+
+	if (image1.width !== image2.width) return false
+	if (image1.height !== image2.height) return false
+
+	return image1.hash.equals(image2.hash)
+}
+
+export default CompareImage
